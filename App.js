17045cabb1fa73ec1f233a10f9918edead26a2bc
Gayathri Thiruvayipati <gayathrithiruvayipati@Gayathris-MacBook-Air.local>
2025-06-17 16:47:13 -0700

    parcel

--- App.js
+++ App.js
@@ -9,14 +9,17 @@
  *  ReactElement(Object) => HTML(Browser Understands)
  */
 
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm h1 tag"),
-    React.createElement("h2", {}, "I'm h2 tag"),
+  React.createElement("div", { id: "child", key: "child" }, [
+    React.createElement("h1", { key: "child-h1" }, "I'm h1 tag"),
+    React.createElement("h2", { key: "child-h2" }, "I'm h2 tag"),
   ]),
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "I'm h1 tag"),
-    React.createElement("h2", {}, "I'm h2 tag"),
+  React.createElement("div", { id: "child1", key: "child1" }, [
+    React.createElement("h1", { key: "child1-h1" }, "I'm h1 tag"),
+    React.createElement("h2", { key: "child1-h2" }, "I'm h2 tag"),
   ]),
 ]);
 
---
+++ README.md
@@ -0,0 +1,23 @@
+# Notes
+
+- files that can be generated shouldn't be pushed to github repository.
+
+# Parcel
+
+1. Dev Build
+2. Local Server
+3. HMP = Hot Module Replacement
+4. File Watching Algorithm - written in c++
+5. Caching - Faster Builds
+6. Image Optimization
+7. Minification
+8. Bundling
+9. Compress
+10. Consistent Hashing
+11. Code splitting
+12. Differential bundling - support older browsers
+13. Diagnostic
+14. Error Handling
+15. HTTPS
+16. Tree Shaking - remove unused code
+17. Different dev and prod bundles
