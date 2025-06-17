9cde07ff5fa4aadd94598136bef725f506dce873
Gayathri Thiruvayipati <gayathrithiruvayipati@Gayathris-MacBook-Air.local>
2025-06-16 23:37:40 -0700

    react create elements

---
+++ App.js
@@ -0,0 +1,25 @@
+/**
+ * <div id="parent">
+ *      <div id="child">
+ *          <h1></h1>
+ *          <h2></h2>
+ *      </div>
+ * </div>
+ *
+ *  ReactElement(Object) => HTML(Browser Understands)
+ */
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I'm h1 tag"),
+    React.createElement("h2", {}, "I'm h2 tag"),
+  ]),
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "I'm h1 tag"),
+    React.createElement("h2", {}, "I'm h2 tag"),
+  ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
